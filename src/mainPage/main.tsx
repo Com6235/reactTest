4299a2924d7b2078c26db8551a807d84794cbcdc
Com6235 <ru.nms199@gmail.com>
2024-06-23 12:46:07 +0300

    Update tailwind.css

--- src/mainPage/main.tsx
+++ src/mainPage/main.tsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
 import {Page} from './Page.tsx'
-import '../assets/taiwind.css'
+import '../assets/tailwind.css'
 import { Wrapper } from '../assets/common.tsx'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
